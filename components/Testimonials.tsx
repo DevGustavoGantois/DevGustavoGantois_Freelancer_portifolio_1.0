54aab3f0570d40909177d81c86bd5637d47156c2
Gustavo Gantois <gugagantois@gmail.com>
2025-03-27 19:37:18 -0300

    feat:Adding new testimonials.

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -11,7 +11,9 @@ const TestimonialsData = [
     {name: "Mônica Aran", description: "Ficou show Gustavo! O site ficou perfeito e transmitiu o que eu quero passar para os meus clientes!"},
     {name: "Eric Souza dos Santos", description: "Trabalhar com Gustavo no Hackathon da FullDev foi essencial para o sucesso do projeto. Como designer UI/UX e desenvolvedor front-end, ele criou uma identidade visual original e de alta qualidade usando React e Next.js. Proativo, comunicativo e dedicado, sua contribuição foi fundamental para conquistarmos o primeiro lugar. Recomendo Gustavo sem dúvida para qualquer desafio!"},
     {name: "Carlos Augusto", description: "Colaborar com Gustavo no Hackathon Ato 5 da Fulldev foi incrível! Sua visão criativa e habilidade em transformar ideias em interfaces intuitivas elevaram o projeto. Tenho certeza de que ele fará a diferença em qualquer equipe!"},
-    {name: "Gabriel Dias Santiago", description: "Minha sincera gratidão ao Gustavo pelo excelente trabalho como designer e desenvolvedor front-end. Sua criatividade e técnica foram impecáveis. Foi um prazer trabalhar com você e espero colaborarmos novamente no futuro!"}
+    {name: "Gabriel Dias Santiago", description: "Minha sincera gratidão ao Gustavo pelo excelente trabalho como designer e desenvolvedor front-end. Sua criatividade e técnica foram impecáveis. Foi um prazer trabalhar com você e espero colaborarmos novamente no futuro!"},
+    {name: "Camila Oliveira", description: "Simplismente amei, entrega de qualidade, 100% atencioso e solicito desde o início até o final do desenvolvimento do meu site."},
+    {name: "Rafael Perrone", description: "Irmão, gostei muito do seu trabalho, muito solicito, muito obrigado mesmo o seu trabalho é top, design e desenvolvimento impecáveis."}
 ];
 
 export function Testimonials() {
